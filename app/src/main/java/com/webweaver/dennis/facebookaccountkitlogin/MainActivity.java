5776061fe4a4883bb858b19214b9f55720753c78
dennismuturia <dmuturia2922@gmail.com>
2018-04-26 12:39:03 +0300

    Declared the buttons on the main activity.

--- app/src/main/java/com/webweaver/dennis/facebookaccountkitlogin/MainActivity.java
+++ app/src/main/java/com/webweaver/dennis/facebookaccountkitlogin/MainActivity.java
@@ -2,12 +2,19 @@ package com.webweaver.dennis.facebookaccountkitlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static int APP_REQUEST_CODE = 99;
+    Button btnPhone, btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declaring the buttons that I am using.
+        btnEmail = (Button)findViewById(R.id.emailButton);
+        btnPhone = (Button)findViewById(R.id.phoneButton);
     }
 }
